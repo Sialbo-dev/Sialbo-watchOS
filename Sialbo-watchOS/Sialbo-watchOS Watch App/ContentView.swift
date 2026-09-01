4aebd092c7ababfda6010286236cfb92c1c922b4
Juhui <joohee090331@gmail.com>
2026-09-02 00:47:12 +0900

    🔨 :: Xcode 프로젝트 초기 생성

---
+++ Sialbo-watchOS/Sialbo-watchOS Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Sialbo-watchOS Watch App
+//
+//  Created by 신주희 on 9/1/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Sialbo-watchOS/Sialbo-watchOS Watch App/Sialbo_watchOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sialbo_watchOSApp.swift
+//  Sialbo-watchOS Watch App
+//
+//  Created by 신주희 on 9/1/26.
+//
+
+import SwiftUI
+
+@main
+struct Sialbo_watchOS_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
