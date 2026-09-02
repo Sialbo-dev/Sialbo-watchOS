4684eb811f584533afe5271bd47da7b2dd54006a
Juhui <joohee090331@gmail.com>
2026-09-03 00:16:52 +0900

    ✨ :: 로고 헤더 컴포넌트 분리

---
+++ Sialbo-watchOS/Sialbo-watchOS Watch App/Views/Components/LogoHeaderView.swift
@@ -0,0 +1,28 @@
+//
+//  LogoHeaderView.swift
+//  Sialbo-watchOS Watch App
+//
+//  화면 상단 로고 + 타이틀, 재사용 헤더
+//
+
+import SwiftUI
+
+struct LogoHeaderView: View {
+    var title: String = "시얼보"
+
+    var body: some View {
+        HStack(spacing: 4) {
+            Image("Logo")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 19, height: 20)
+            Text(title)
+                .font(.griun(16))
+                .foregroundStyle(.logoGold)
+        }
+    }
+}
+
+#Preview {
+    LogoHeaderView()
+}
