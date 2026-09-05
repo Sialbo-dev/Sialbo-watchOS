7e619e01e09972c5b038b9faca07314f9e29f728
Juhui <joohee090331@gmail.com>
2026-09-05 16:19:58 +0900

    🔨 :: 위젯 확장 타깃 추가

---
+++ Sialbo-watchOS/Sialbo-watchOS-Widget/Sialbo_watchOS_WidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  Sialbo_watchOS_WidgetBundle.swift
+//  Sialbo-watchOS-Widget
+//
+//  Created by 신주희 on 9/5/26.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct Sialbo_watchOS_WidgetBundle: WidgetBundle {
+    var body: some Widget {
+        Sialbo_watchOS_Widget()
+    }
+}
